5f0bddbab391f41c764c6e8d4abe02f7013fc927
LabdhPurohit <80207914+LabdhPurohit@users.noreply.github.com>
2023-01-17 14:21:58 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@#fop&1mkrdpp2hx0w%%fez4-e*&tqnhvu9(%u60$_#&=zplfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
